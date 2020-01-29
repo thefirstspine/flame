dac37ee367dd8136ed6b93c159cf297eddf292bc
The First Spine <59167793+thefirstspine@users.noreply.github.com>
2020-01-29 18:04:43 +0100

    Update compute-players.py

--- compute-players.py
+++ compute-players.py
@@ -1,18 +1,19 @@
 import fire
 import json
 import os
+from base import FireCommand
 
 
-class ComputePlayers(object):
-    def get_player(self, player_id, env='production'):
+class ComputePlayers(FireCommand):
+    def get_player(self, player_id, env='production', export_json=False):
         """"Get a player data"""
-        with open(self.__get_data_path(env) + '/wizzards/' + str(player_id), 'r') as outfile:
+        with open(self.get_data_path(env) + '/wizzards/' + str(player_id), 'r') as outfile:
             data = outfile.read()
-            return json.loads(data)
+            return data if export_json is True else json.loads(data)
 
-    def ranks(self, env='production'):
+    def ranks(self, env='production', export_json=False):
         """Ranks the players according to their victories"""
-        files = os.listdir(self.__get_data_path(env) + '/wizzards')
+        files = os.listdir(self.get_data_path(env) + '/wizzards')
         ranks = []
         for name in files:
             player_data = self.get_player(name, env)
@@ -24,23 +25,21 @@ class ComputePlayers(object):
                 },
                 'points': points,
             })
-        return sorted(ranks, key=lambda x: x['points'], reverse=True)
+        output = sorted(ranks, key=lambda x: x['points'], reverse=True)
+        return json.dumps(output) if export_json is True else output
 
-    def points(self, player_id, env='production'):
+    def get_points(self, player_id, env='production', export_json=False):
         """Get the points of a player"""
         player_data = self.get_player(player_id, env)
-        return self.__compute_points(player_data['history'])
+        output = self.__compute_points(player_data['history'])
+        return json.dumps(output) if export_json is True else output
 
-    def __compute_points(self, history):
+    def __compute_points(self, history, export_json=False):
         """Compute the points from an history list"""
         points = 0
         for history_item in history:
             points += 3 if history_item['victory'] else -1
-        return points
-
-    def __get_data_path(self, env):
-        """Get the data path according to an environment"""
-        return '/storage/arena' if env == 'production' else './data'
+        return json.dumps(points) if export_json is True else points
 
 
 if __name__ == '__main__':
